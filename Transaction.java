643bbf777f490aab3ad385b1c919b3922e976825
DragoPowder <liemnuwen@gmail.com>
2024-11-13 15:36:23 -0600

    transaction object

---
+++ Transaction.java
@@ -0,0 +1,75 @@
+/*
+
+
+
+*/
+public Class Transaction{
+
+    private boolean type;
+    private String category;
+    private double amount;
+    private String date;
+
+    public Transaction(){
+        type = true;
+        category = "";
+        amount = 0.0;
+        date = "NaN";
+    }
+
+    public Transaction(boolean, isDeposit, String descript, double paid, String date){
+        type = isDeposit;
+        category = descript;
+        amount = paid;
+        this.date = mmddyyyy
+    }
+
+    //return methods
+    public String getType(){
+         if(type){
+            return "deposit";
+        }else{
+            return "expense";
+        }
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    //edit methods
+    public void editType(boolean isDeposit){
+        type = isDeposit;
+    }
+
+    public void editCategory(String descript){
+        category = descript;
+    }
+
+    public void editAmount(double paid){
+        amount = paid;
+    }
+
+    public void editDate(String date){
+        this.date = date;
+    }
+
+    public String toString(){
+        String str = "$" + amount;
+
+        if(type){
+            str += " deposited ";
+        }else{
+            str += " expended ";
+        }
+        str += "on " + date + " for " + category ".";
+    }
+}
