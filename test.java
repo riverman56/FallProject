8cf6e4ebd2c7c274544137e69d70357d015525c5
riverman56 <79799463+riverman56@users.noreply.github.com>
2024-11-13 13:46:54 -0600

    test

---
+++ test.java
@@ -0,0 +1,14 @@
+public class Fart
+{
+    private int windPower;
+
+    public Fart(int windPower)
+    {
+        windPower = windPower;
+    }
+
+    public void toot()
+    {
+        windPower--;
+    }
+}
